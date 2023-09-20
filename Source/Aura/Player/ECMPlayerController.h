b0d5e3fae581558b971cc303063894a26935d3f9
cvonayres <carl.ayres@live.co.uk>
2023-09-20 19:43:59 +0100

    Add goblin and player contoller

---
+++ Source/Aura/Player/ECMPlayerController.h
@@ -0,0 +1,14 @@
+// Copyright Electronic CAD Monkey [ECM]
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "ECMPlayerController.generated.h"
+
+UCLASS()
+class AURA_API AECMPlayerController : public APlayerController
+{
+	GENERATED_BODY()
+	
+};
