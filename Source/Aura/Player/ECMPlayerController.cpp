689d921559d087a013eae4bc66cba793fac781f4
cvonayres <carl.ayres@live.co.uk>
2023-09-21 00:08:30 +0100

    Added Camera Manager

--- Source/Aura/Player/ECMPlayerController.cpp
+++ Source/Aura/Player/ECMPlayerController.cpp
@@ -1,5 +1,68 @@
 // Copyright Electronic CAD Monkey [ECM]
 
-
 #include "ECMPlayerController.h"
+#include "ECMPlayerCameraManager.h"
+#include "EnhancedInputSubsystems.h"
+#include  "EnhancedInputComponent.h"
 
+AECMPlayerController::AECMPlayerController()
+{
+	bReplicates = true;
+}
+
+void AECMPlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+
+	// Add Input Mapping Context
+	check(InputContext);
+	UEnhancedInputLocalPlayerSubsystem*  Subsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(GetLocalPlayer());
+	check(Subsystem);
+	Subsystem->AddMappingContext(InputContext,0);
+
+	// Setup Input Mode 
+	FInputModeGameAndUI InputModeData;
+	InputModeData.SetLockMouseToViewportBehavior(EMouseLockMode::DoNotLock);
+	InputModeData.SetHideCursorDuringCapture(false);
+	SetInputMode(InputModeData);
+
+	// Setup cursor
+	bShowMouseCursor=true;
+	DefaultMouseCursor = EMouseCursor::Default;
+
+	CameraManager =  Cast<AECMPlayerCameraManager>(PlayerCameraManager);
+}
+
+// Cast InputComponent and bind Input Actions
+void AECMPlayerController::SetupInputComponent()
+{
+	Super::SetupInputComponent();
+
+	UEnhancedInputComponent* EnhancedInputComponent = CastChecked<UEnhancedInputComponent>(InputComponent);
+
+	EnhancedInputComponent->BindAction(MoveAction, ETriggerEvent::Triggered, this, &AECMPlayerController::Move);
+	EnhancedInputComponent->BindAction(ZoomAction, ETriggerEvent::Triggered, this, &AECMPlayerController::ZoomCamera);
+}
+
+// Move Function
+void AECMPlayerController::Move(const FInputActionValue& InputActionValve)
+{
+	const FVector2d InputAxisVector = InputActionValve.Get<FVector2d>();
+	const FRotator Rotation = GetControlRotation();
+	const FRotator YawRotation (0.f, Rotation.Yaw, 0.f);
+	
+	const FVector ForwardDirection = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::X);
+	const FVector RightDirection = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::Y);
+
+	if (APawn* ControlledPawn = GetPawn<APawn>())
+	{
+		ControlledPawn->AddMovementInput(ForwardDirection,InputAxisVector.Y);
+		ControlledPawn->AddMovementInput(RightDirection,InputAxisVector.X);
+	}
+}
+
+// Zoom Camera
+void AECMPlayerController::ZoomCamera(const FInputActionValue& InputActionValve)
+{
+	CameraManager->UpdateZoom(InputActionValve.Get<float>());
+}
--- Source/Aura/Player/ECMPlayerController.h
+++ Source/Aura/Player/ECMPlayerController.h
@@ -6,9 +6,34 @@
 #include "GameFramework/PlayerController.h"
 #include "ECMPlayerController.generated.h"
 
+class UInputMappingContext;
+class UInputAction;
+struct FInputActionValue;
+
 UCLASS()
 class AURA_API AECMPlayerController : public APlayerController
 {
 	GENERATED_BODY()
+public:
+	AECMPlayerController();
 	
+protected:
+	virtual void BeginPlay() override;
+	virtual void SetupInputComponent() override;
+	
+private:
+	UPROPERTY(VisibleAnywhere, Category="Referances")
+	class AECMPlayerCameraManager* CameraManager;
+	
+	UPROPERTY(EditAnywhere, Category="Input")
+	TObjectPtr<UInputMappingContext> InputContext;
+
+	UPROPERTY(EditAnywhere, Category="Input")
+	TObjectPtr<UInputAction> MoveAction;
+	UPROPERTY(EditAnywhere, Category="Input")
+	TObjectPtr<UInputAction> ZoomAction;
+
+	void Move(const FInputActionValue& InputActionValve);
+	void ZoomCamera(const FInputActionValue& InputActionValve);
+
 };
